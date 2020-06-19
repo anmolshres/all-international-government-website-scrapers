85f97e65dc8ea73cdd2a39fa6612cd690922c9b2
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-06-19 01:39:32 -0400

    add brazil to unix script

--- all_scrapes/unix_script.sh
+++ all_scrapes/unix_script.sh
@@ -3,6 +3,9 @@ if [ $# != 1 ]; then
 elif [ "${1,,}" == "cdc" ]; then
     scrapy crawl cdc_links
     scrapy crawl cdc_posts -o posts/cdc_posts.json
+elif [ "${1,,}" == "brazil" ]; then
+    scrapy crawl brazil_links
+    scrapy crawl brazil_posts -o posts/brazil_posts.json
 else
     printf "Uh-Oh! Sorry, that region does not have any script for it. \nCheck README for all available options.\n"
 fi
