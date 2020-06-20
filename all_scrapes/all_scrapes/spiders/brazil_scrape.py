86c25051e74f48bb48735c75056574b81ad2f830
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-06-20 02:16:12 -0400

    handle edge case links

--- all_scrapes/all_scrapes/spiders/brazil_scrape.py
+++ all_scrapes/all_scrapes/spiders/brazil_scrape.py
@@ -27,10 +27,12 @@ class PostsSpider(scrapy.Spider):
         updatedDateTime = str(updatedDateISO)+'T'+updatedTimeISO
         title = response.css('title::text').get()
         contentArray = response.css('.item-page > p').extract()
+        contentArray = response.css('.item-pagenoticias > p').extract() if len(contentArray) == 0 else contentArray
+        contentArray = response.css('.item-page::text').extract() if len(contentArray) == 0 else contentArray
         converter = html2text.HTML2Text()
         converter.ignore_links = True
         text = reduce(lambda first, second: converter.handle(
-            first)+converter.handle(second), contentArray[:len(contentArray)-2])
+            first)+converter.handle(second), contentArray)
         isReliable, textBytesFound, details = cld2.detect(text)
         language = details[0].language_name
         yield{
