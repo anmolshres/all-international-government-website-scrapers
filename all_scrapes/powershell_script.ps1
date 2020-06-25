449b616bc9b8430133649060cd343332283eddac
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2020-06-25 01:04:24 -0400

    update scripts

--- all_scrapes/powershell_script.ps1
+++ all_scrapes/powershell_script.ps1
@@ -9,6 +9,10 @@ elseif ($args -eq "brazil".ToLower()) {
     scrapy crawl brazil_links
     scrapy crawl brazil_posts -o posts/brazil_posts.json
 }
+elseif ($args -eq "spain".ToLower()) {
+    scrapy crawl spain_links
+    scrapy crawl spain_posts -o posts/spain_posts.json
+}
 else {
     Write-Host("Uh-Oh! Sorry, that region does not have any script for it. `nCheck README for all available options.")
 }
--- all_scrapes/unix_script.sh
+++ all_scrapes/unix_script.sh
@@ -6,6 +6,9 @@ elif [ "${1,,}" == "cdc" ]; then
 elif [ "${1,,}" == "brazil" ]; then
     scrapy crawl brazil_links
     scrapy crawl brazil_posts -o posts/brazil_posts.json
+elif [ "${1,,}" == "spain" ]; then
+    scrapy crawl spain_links
+    scrapy crawl spain_posts -o posts/spain_posts.json
 else
     printf "Uh-Oh! Sorry, that region does not have any script for it. \nCheck README for all available options.\n"
 fi
